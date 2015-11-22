5f69530ba4bc87633eb309d9b29263138db22742
Nicholas Stewart <nicholas4@gmail.com>
2015-11-22 11:07:18 -0700

    Refactoring

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 iterations = 1_000_000
 
-puts "Keys are strings"
-# Time to create users with keys as strings
+message = "How long does it take to create #{iterations} hashes, when keys are strings?"
+puts message
 time_begin = Time.now
 iterations.times do
   {"first_name" => "Fred", "last_name" => "Smith"}
@@ -9,11 +9,11 @@ end
 
 elapsed_time_1 = Time.now - time_begin
 puts "Time: #{elapsed_time_1} seconds"
-
 puts ""
 
-puts "Keys are symbols"
-# Time to create users with keys as strings
+
+message = "How long does it take to create #{iterations} hashes, when keys are symbols?"
+puts message
 time_begin = Time.now
 iterations.times do
   {:first_name => "Fred", :last_name => "Smith"}
@@ -21,7 +21,8 @@ end
 
 elapsed_time_2 = Time.now - time_begin
 puts "Time: #{elapsed_time_2} seconds"
-
 puts ""
 
+
 puts "Using symbols is #{elapsed_time_1 / elapsed_time_2} times faster"
+puts ""
